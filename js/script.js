2b9475cf87eaca2f0ecbde1e2a049762a3bb9c31
Ashley Rapone <ashleyrapone@Ashleys-MacBook-Air.local>
2021-04-24 13:01:13 -0400

    Initial Files

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [
+                { quote: "A mind needs books as a sword needs a whetstone, if it is to keep its edge.",
+                  source: "Tyrion Lannister",
+                  citation: "Game of Thrones",
+                  year: 2011}, 
+                { quote: "People often claim to hunger for truth, but seldom like the taste when it's served up.",
+                  source: "Tyrion Lannister",
+                  citation: "Game of Thrones"}, 
+                { quote: "When you tear out a man's tongue, you are not proving him a liar, you're only telling the world that you fear what he might say.",
+                  source: "Tyrion Lannister",
+                  citation: "Game of Thrones"}, 
+                { quote: "Death is so terribly final, while life is full of possibilities.",
+                  source: "Tyrion Lannister",
+                  citation: "Game of Thrones"}, 
+                { quote: "Why is it that when one man builds a wall, the next man immediately needs to know what's on the other side?",
+                  source: "Tyrion Lannister",
+                  citation: "Game of Thrones"}
+]
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
