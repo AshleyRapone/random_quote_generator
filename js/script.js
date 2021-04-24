22810a47122bcc52b0293576659276b5dc645755
Ashley Rapone <ashleyrapone@Ashleys-MacBook-Air.local>
2021-04-24 17:17:06 -0400

    added getRandomQuote and printQuote

--- js/script.js
+++ js/script.js
@@ -14,32 +14,44 @@ let quotes = [
                 { quote: "A mind needs books as a sword needs a whetstone, if it is to keep its edge.",
                   source: "Tyrion Lannister",
                   citation: "Game of Thrones",
-                  year: 2011}, 
+                  year: 2011 }, 
                 { quote: "People often claim to hunger for truth, but seldom like the taste when it's served up.",
                   source: "Tyrion Lannister",
-                  citation: "Game of Thrones"}, 
+                  citation: "Game of Thrones" }, 
                 { quote: "When you tear out a man's tongue, you are not proving him a liar, you're only telling the world that you fear what he might say.",
                   source: "Tyrion Lannister",
-                  citation: "Game of Thrones"}, 
+                  citation: "Game of Thrones" }, 
                 { quote: "Death is so terribly final, while life is full of possibilities.",
                   source: "Tyrion Lannister",
-                  citation: "Game of Thrones"}, 
+                  citation: "Game of Thrones" }, 
                 { quote: "Why is it that when one man builds a wall, the next man immediately needs to know what's on the other side?",
                   source: "Tyrion Lannister",
-                  citation: "Game of Thrones"}
+                  citation: "Game of Thrones" }
 ]
-
-
 /***
  * `getRandomQuote` function
 ***/
-
-
+function getRandomQuote() {
+  let quotesLength = quotes.length;
+  let randomNumber = Math.floor(Math.random() * quotesLength);
+  return quotes[randomNumber];
+}
 
 /***
  * `printQuote` function
 ***/
-
+function printQuote() {
+  let randomQuote = getRandomQuote();
+  let html = `<p class="quote"> ${ randomQuote.quote } </p><p class="source"> ${ randomQuote.source }`;
+  if (randomQuote.citation) {
+    html += `<span class="citation"> ${ randomQuote.citation } </span>`;
+  }
+  if (randomQuote.year) {
+    html += `<span class="year"> ${ randomQuote.year } </span>`;
+  }
+  html += '</p>'
+  return document.getElementById('quote-box').innerHTML = html;
+}
 
 
 /***
